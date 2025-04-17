cdf064eb431b7f43eee0ed1f7e9ce20406e03b37
daang04 <117044840+daang04@users.noreply.github.com>
2025-04-17 14:38:29 -0500

    daaaaaaaaa

--- vista_datos.py
+++ vista_datos.py
@@ -17,7 +17,6 @@ authenticator = stauth.Authenticate(
     config['cookie']['name'],
     config['cookie']['key'],
     config['cookie']['expiry_days'],
-    config['preauthorized']
 )
 
 # Realizar el login
