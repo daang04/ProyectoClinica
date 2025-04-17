0d5bf33a8418d6ff4b30e517d6014fc3dcaa37fe
daang04 <117044840+daang04@users.noreply.github.com>
2025-04-17 13:49:11 -0500

    a

--- vista_datos.py
+++ vista_datos.py
@@ -24,7 +24,7 @@ authenticator = stauth.Authenticate(
     "mi_aplicacion", "clave_firma", cookie_expiry_days=1
 )
 
-nombre_usuario, autenticado, nombre_rol = authenticator.login("Iniciar sesión", "main")
+nombre_usuario, autenticado, nombre_rol = authenticator.login("Iniciar sesión", location="main")
 
 if not autenticado:
     st.warning("Por favor, inicia sesión para continuar.")
