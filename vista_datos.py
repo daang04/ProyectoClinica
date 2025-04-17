866ffb599b64018dc4b9436d1923b7fe25338610
daang04 <117044840+daang04@users.noreply.github.com>
2025-04-17 13:40:33 -0500

    usuarios

--- vista_datos.py
+++ vista_datos.py
@@ -3,10 +3,34 @@ from streamlit_option_menu import option_menu
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+import streamlit_authenticator as stauth
 from base_datos import mostrar_base_datos
 from generar_qr import generar_qrs
 
-# ---------- AUTENTICACIÓN ----------
+
+nombres = st.secrets["auth"]["nombres"]
+usuarios = st.secrets["auth"]["usuarios"]
+contrasenas = st.secrets["auth"]["contrasenas"]
+
+credentials = {
+    "usernames": {
+        user: {"name": nombre, "password": hash_}
+        for user, nombre, hash_ in zip(usuarios, nombres, contrasenas)
+    }
+}
+
+authenticator = stauth.Authenticate(
+    credentials,
+    "mi_aplicacion", "clave_firma", cookie_expiry_days=1
+)
+
+nombre_usuario, autenticado, nombre_rol = authenticator.login("Iniciar sesión", "main")
+
+if not autenticado:
+    st.warning("Por favor, inicia sesión para continuar.")
+    st.stop()
+
+# ---------- AUTENTICACIÓN GOOGLE SHEETS ----------
 info = st.secrets["google_service_account"]
 scope = ['https://www.googleapis.com/auth/spreadsheets',
          'https://www.googleapis.com/auth/drive']
@@ -14,6 +38,9 @@ credenciales = ServiceAccountCredentials.from_json_keyfile_dict(info, scope)
 cliente = gspread.authorize(credenciales)
 
 # ---------- MENÚ ----------
+authenticator.logout("Cerrar sesión", "sidebar")
+st.sidebar.success(f"👋 Bienvenido, {nombre_usuario}")
+
 with st.sidebar:
     menu = option_menu(
         "Menú principal",
@@ -22,6 +49,7 @@ with st.sidebar:
         menu_icon="cast",
         default_index=0
     )
+
 if menu == "Inicio":
     st.title("🏥 Bienvenido al Sistema de Inventario")
     st.write("Navega usando el menú lateral para ver y gestionar los equipos médicos.")
